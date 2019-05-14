36981af2ed9eeb13c4bf3bc93f9b44ca4063c849
tauroff <pajuodis.tauras@gmail.com>
2019-05-14 06:26:54 +0100

    Group chat button removed

--- Instant Messenger/src/mainApplication.java
+++ Instant Messenger/src/mainApplication.java
@@ -1,21 +1,21 @@
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
 
 public class mainApplication extends JFrame {
 
 	private JPanel contentPane;
+	private String username;
 
 	/**
 	 * Launch the application.
 	 */
+	/*
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -27,12 +27,13 @@ public class mainApplication extends JFrame {
 				}
 			}
 		});
-	}
+	}*/
 
 	/**
 	 * Create the frame.
 	 */
-	public mainApplication() {
+	public mainApplication(String username) {
+		this.username = username;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -40,26 +41,15 @@ public class mainApplication extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		// Login button
-		JButton btnAppGroupChat = new JButton("Group chat");
-		btnAppGroupChat.setFont(new Font("Lucida Grande", Font.BOLD, 24));
-		btnAppGroupChat.setBounds((450/2)-120, 5, 240, 60);
-		btnAppGroupChat.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				groupChat groupChat = new groupChat();
-			}
-		});
-		contentPane.setLayout(null);
-		contentPane.add(btnAppGroupChat);
+
 				
-		// Register button
+		// Connection information button
 		JButton btnAppP2P = new JButton("Peer-to-peer");
 		btnAppP2P.setFont(new Font("Lucida Grande", Font.BOLD, 24));
 		btnAppP2P.setBounds((450/2)-120, 70, 240, 60);
 		btnAppP2P.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//peerToPeer peerToPeer = new peerToPeer();
-				connectionInfo connInfo = new connectionInfo();
+				connectionInfo connInfo = new connectionInfo(username);
 			}
 		});
 		contentPane.add(btnAppP2P);
